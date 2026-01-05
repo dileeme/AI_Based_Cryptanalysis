611e662890f776d42f24a2369ea960bc4c45312e
dilen shankar <elchurrocuandotebese@gmail.com>
2026-01-05 13:00:23 +0400

    Update freq_analysis.py

--- AI cryptanalysis/freq_analysis.py
+++ AI cryptanalysis/freq_analysis.py
@@ -1,1 +1,16 @@
+from collections import Counter
+
+english_freq="ETAOINSHRDLCUMWFGYPBVKJXQZ"
+
+def freq_attack(cipher):
+    c=Counter(cipher)
+    order=[x[0] for x in c.most_common()]
+    m={}
+    for i in range(len(order)):
+        m[order[i]]=english_freq[i]
+    return "".join(m.get(ch,ch) for ch in cipher)
+
+if __name__=="__main__":
+    cipher="GSRHRZVWNVLMHGIZGRLM"
+    print(freq_attack(cipher))
 
