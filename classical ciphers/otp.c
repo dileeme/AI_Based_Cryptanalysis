f10b552014483ff7a8beb82a41735f84acaf7969
dilen shankar <elchurrocuandotebese@gmail.com>
2026-01-05 13:00:45 +0400

    Update otp.c

--- classical ciphers/otp.c
+++ classical ciphers/otp.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+char t[]="PARKTHECARATAARDVARKYARD";
+char k[]="XMCKLXMCKLXMCKLXMCKLXM";
+int i;
+for(i=0;i<strlen(t);i++)printf("%c",(t[i]-'A'+k[i]-'A')%26+'A');
+}
 
